efb14a86919bcaf1e34a0432d402f662acb8faf2
yexm <yexingmin06221@126.com>
2023-04-19 00:13:39 +0800

    Initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yexm/railway
 
 go 1.17
+
+require github.com/davecgh/go-spew v1.1.1
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		spew.Dump(r)
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
